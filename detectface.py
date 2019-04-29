771230f27300863ed9799b3405972aa5e2770586
Fred Nielsen <16697069+fredless@users.noreply.github.com>
2019-04-28 21:24:28 -0700

    added license and module docstring

--- detectface.py
+++ detectface.py
@@ -1,3 +1,24 @@
+# Copyright (C) 2019 Frederick W. Nielsen
+#
+# This file is part of headshotTools.
+#
+# headshotTools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# headshotTools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with headshotTools.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Takes a path as single argument, then processes files w/ OpenCV to output "headshot" ready files
+"""
+
 import os
 import sys
 import cv2
